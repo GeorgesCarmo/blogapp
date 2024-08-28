a8081aa41c80fe2766cfc4b466274b5c87230239
Georges do Carmo Pereira <139582154+GeorgesCarmo@users.noreply.github.com>
2024-08-28 12:36:32 -0300

    Add model Postagem
    página para criar nova postagem

--- routes/admin.js
+++ routes/admin.js
@@ -102,4 +102,22 @@ router.post("/categorias/deletar", (req, res)=>{
     })
 })
 
+router.get("/postagens", (req, res)=>{
+    res.render("admin/postagens")
+})
+
+router.get("/postagens/add", (req, res)=>{
+    Categoria.find().lean()
+    .then((categorias)=>{
+        res.render("admin/addpostagem", {categorias:categorias})
+    }).catch((err)=>{
+        req.flash("error_msg", "Erro ao carregar o formulario")
+        res.redirect("/admin")
+    })
+})
+
+router.post("/postagens/nova", (req, res)=>{
+    
+})
+
 module.exports = router
