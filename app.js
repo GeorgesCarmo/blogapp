613afeabfbfeda5a79db1c1118057bddb8dcc273
Georges do Carmo Pereira <139582154+GeorgesCarmo@users.noreply.github.com>
2024-08-29 13:07:29 -0300

    add login e passport

--- app.js
+++ app.js
@@ -2,17 +2,19 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const app = express()
 const admin = require('./routes/admin')
 const path = require('path')
 const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('connect-flash')
-const app = express()
 require("./models/Postagem")
 const Postagem = mongoose.model("postagens")
 require("./models/Categoria")
 const Categoria = mongoose.model("categorias")
 const usuarios = require('./routes/usuario')
+const passport = require("passport")
+require("./config/authent")(passport)
 
 /*------------------------------------CONFIGURAÇÕES-------------------------------------*/
     // bodyParser
@@ -42,12 +44,17 @@ const usuarios = require('./routes/usuario')
             resave: true,
             saveUninitialized: true
         }))
+
+        app.use(passport.initialize())
+        app.use(passport.session())
+
         app.use(flash())
 
     // middleware
         app.use((req, res, next)=>{
             res.locals.success_msg = req.flash("success_msg")
             res.locals.error_msg = req.flash("erro_msg")
+            res.locals.error = req.flash("error")
             next()
         }) 
    
